47aa10fecd18895f193b68bb6af6abdaec1f8fb8
Siarhei B <sergeybednyuk@gmail.com>
2026-03-20 11:21:41 +0100

    docs[app]: add component lifecycle steps interaction for learning porposes

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, DestroyRef, inject, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-server-status',
-  standalone: true,
-  imports: [],
-  templateUrl: './server-status.component.html',
-  styleUrl: './server-status.component.css'
+    selector: 'app-server-status',
+    standalone: true,
+    imports: [],
+    templateUrl: './server-status.component.html',
+    styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
-    currentStatus = 'online';
+export class ServerStatusComponent implements OnInit, AfterViewInit {
+    // private interval?: ReturnType<typeof setInterval>;
+    private destroyRef = inject(DestroyRef);
+    currentStatus: 'online' | 'offline' | 'unknown' = 'online';
+
+    constructor() {
+    }
+
+    ngOnInit() {
+        console.log('ON INIT');
+        const interval = setInterval(() => {
+            const rnd = Math.random();
+
+            if (rnd > 0.5) {
+                this.currentStatus = 'online';
+            } else if (rnd < 0.9) {
+                this.currentStatus = 'offline';
+            } else {
+                this.currentStatus = 'unknown';
+            }
+
+        }, 5000);
+
+        this.destroyRef.onDestroy(() => {
+            clearInterval(interval);
+        });
+    }
+
+    ngAfterViewInit(): void {
+        console.log('AFTER VIEW INIT');
+    }
+
+    // Old approach
+    // ngOnDestroy(): void {
+    //     clearTimeout(this.interval);
+    // }
 }
