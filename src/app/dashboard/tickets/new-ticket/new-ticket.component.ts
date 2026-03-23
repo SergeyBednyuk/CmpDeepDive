42caba3976eddb0ef739c4e118e3578a4da2ef6b
Siarhei B <sergeybednyuk@gmail.com>
2026-03-23 13:23:20 +0100

    docs[new-ticket]: implement OnInit, AfterViewInit, AfterRender and AfterNextRender for learning porposes

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,15 @@
-import { Component, ContentChild, contentChild, ElementRef, viewChild, ViewChild } from '@angular/core';
+import {
+    afterNextRender,
+    afterRender,
+    AfterViewInit,
+    Component,
+    ContentChild,
+    contentChild,
+    ElementRef,
+    OnInit,
+    viewChild,
+    ViewChild
+} from '@angular/core';
 import { FormsModule } from "@angular/forms";
 
 import { ButtonComponent } from "../../../shared/button/button.component";
@@ -15,15 +26,34 @@ import { ControlComponent } from "../../../shared/control/control.component";
     templateUrl: './new-ticket.component.html',
     styleUrl: './new-ticket.component.css'
 })
-export class NewTicketComponent {
+export class NewTicketComponent implements OnInit, AfterViewInit{
     // Old way to template variable
     // @ViewChild('form') form?: ElementRef<HTMLFormElement>;
     // Angular 17.3v+
     private form = viewChild.required<ElementRef<HTMLFormElement>>('form');
 
+    constructor() {
+        afterRender(() => {
+            console.log('AFTER RENDER');
+        });
+        afterNextRender(()=>{
+            console.log('AFTER NEXT RENDER');
+        });
+    }
+
     onSubmit(title: string, ticketText: string) {
         console.log('TITLE TEXT: ' + title);
         console.log('TICKET TEXT: ' + ticketText);
         this.form().nativeElement.reset();
+    }
+
+    ngOnInit() {
+        console.log('ON INIT');
+        console.log(this.form().nativeElement)
+    }
+
+    ngAfterViewInit(): void {
+        console.log('AFTER VIEW INIT');
+        console.log(this.form().nativeElement)
     }
 }
